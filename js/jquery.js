b0106b407360e9ac6fa0566cee056dd8b99070c6
Aiden Luce <luceaiden4124@gmail.com>
2022-10-06 16:10:33 -0500

    updated jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -2,4 +2,7 @@ $(function (){
     $('#dark-mode-button').on("click", function(){
         $('*').toggleClass("dark-mode");
     })
+    $('#chartreuse-mode-button').on("click", function(){
+        $('*').toggleClass("chartreuse-mode");
+    })
 })
